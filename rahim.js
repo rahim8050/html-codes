02da6976e69853e815c043a5e63fdb63352be214
rahim8050 <rahimranxx8050@gmail.com>
2023-02-17 12:40:46 +0300

    commit

---
+++ rahim.js
@@ -0,0 +1,6 @@
+function takevalues()
+            var fname=document.getElementById("firstname").value;
+            var secname=document.getElementById("secondname").value;
+            document.write("your firstname <br> "+" first name :" +fname+"<br>");
+            document.write("your secname<br> "+" secondname :" +secname+"<br>");
+            
